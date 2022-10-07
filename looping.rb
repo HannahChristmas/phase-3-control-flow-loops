ecd7cb396cde10dece88a41ba2fa895d753899c8
HannahChristmas <hc109909@gmail.com>
2022-10-07 18:42:46 -0400

    commit tree 0fd3fa2ef32e2945e088d25755d3356bc2cd309c

--- looping.rb
+++ looping.rb
@@ -1,6 +1,15 @@
+require 'pry'
+
 def happy_new_year
-  # your code here
-end
+  i = 10
+  while i > 0
+    puts i
+    i -= 1
+  end
+    puts "Happy New Year!"
+  end
+
+#Write a method #happy_new_year using a while or until loop that outputs numbers starting at 10 and counting down to 1. After reaching 1, print out "Happy New Year!"
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
 def fizzbuzz(num)
@@ -15,10 +24,26 @@ def fizzbuzz(num)
   end
 end
 
+# binding.pry
+# puts fizzbuzz(3)
+
 def fizzbuzz_printer
   # your code here
+  #for every element of FizzBuzz, print it out until you get to 100
+  # fizzbuzz(1..100).each do |fizzbuzz()| puts fizzbuzz()
+  # end
+
+  (1..100).each do |num|
+    puts fizzbuzz(num)
+  end
+
 end
 
 def reverse_string(str)
+  split_string = str.split('')
+  reversed = []
+  str.size.times { reversed << split_string.pop}
+  reversed.join
   # your code here
 end
+
